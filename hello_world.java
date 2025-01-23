929c7971f24cf4f53382a42c4ce0e6f46986d118
David744823 <744823@hazelwoodschools.org>
2025-01-23 09:15:25 -0600

    Create hello_world.java
    Create hello_world.java
    
    hello_world.java is a simple java program that, using a loop, prints "Hello, World!" 3 times.

---
+++ hello_world.java
@@ -0,0 +1,10 @@
+public class hello_world
+  {
+    public static void main(String[] args)
+    {  
+      for (int loop = 0; loop < 3; loop++) // creates a loop control variable, which starts at 0 and increases each loop until it is equal to or greater than 3. Then, the loop ends
+      {
+        System.out.println("Hello, World!") // prints Hello, World!
+      }
+    } 
+  }
