6808593656562f18cfe8eed1ae7c368106e26959
David744823 <744823@hazelwoodschools.org>
2025-01-23 09:18:15 -0600

    Update hello_world.java
    Fix syntax error
    
    Add a previously missing ; to the end of the print statement (line 7)

--- hello_world.java
+++ hello_world.java
@@ -4,7 +4,7 @@ public class hello_world
     {  
       for (int loop = 0; loop < 3; loop++) // creates a loop control variable, which starts at 0 and increases each loop until it is equal to or greater than 3. Then, the loop ends
       {
-        System.out.println("Hello, World!") // prints Hello, World!
+        System.out.println("Hello, World!"); // prints Hello, World!
       }
     } 
   }
